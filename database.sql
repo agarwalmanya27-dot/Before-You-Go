1a449715296710d08616912a4a91439bc47dda5f
Manya Agarwal <agarwalmanya27@gmail.com>
2026-09-22 20:12:27 +0530

    Added destinations table

--- database.sql
+++ database.sql
@@ -6,3 +6,10 @@ create table users(
      email varchar(100) unique not null,
      password varchar(255) not null,
      created_at timestamp default current_timestamp);
+create table destinations(
+    dest_id int auto_increment primary key,
+    dest_name varchar(50) not null,
+    state varchar(50) not null,
+    vibe enum('Spiritual','Historical','Adventure','Nature','Cultural') not null,
+    descrip text,
+    img_path varchar(255));
